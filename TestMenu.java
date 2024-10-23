6ed1ed615640329d1aa227f9ee5ed1e581cb69e9
HTprog <98377264+HTprog@users.noreply.github.com>
2024-10-23 10:28:44 +0800

    Add files via upload

--- TestMenu.java
+++ TestMenu.java
@@ -5,22 +5,30 @@ public class TestMenu {
 	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
 		MemberArray memarray= new MemberArray("member.txt");
-		memarray.displayMember();
+		EquipmentArray eqarray= new EquipmentArray("equipment.txt");
+		
+		memarray.displayMembers();
 		System.out.println();
 		System.out.println();
 
-		memarray.addMember("Haruhi", "Suzumiya", "HaruSu@utb.edu.bn", "No 34,Jln 190, Spg 10", 32301451, 4519012);
-		memarray.displayMember();
-		System.out.println();
-		System.out.println();
-
-		memarray.saveMembers("member.txt");
-		System.out.println();
-		System.out.println();
-
-		//Thread.sleep(5000);
-		memarray= new MemberArray("member.txt");
-		memarray.displayMember();
+//		memarray.addMember("Haruhi", "Suzumiya", "HaruSu@utb.edu.bn", "No 34,Jln 190, Spg 10", 32301451, 4519012);
+//		memarray.displayMembers();
+//		System.out.println();
+//		System.out.println();
+//
+//		memarray.saveMembers("member.txt");
+//		System.out.println();
+//		System.out.println();
+//
+//		//Thread.sleep(5000);
+//		memarray= new MemberArray("member.txt");
+//		memarray.displayMembers();
+//		System.out.println();
+//		System.out.println();
+		
+	
+		eqarray.displayEquipments();
+		
 
 
 	}
